66827710a243a830def2c81e69b249cebf012a85
Hariel <hariel@hotmail.com>
2022-05-03 11:39:15 -0300

    authService - sign in

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign.in.dto';
 
-@Controller('auth')
+@Controller({ path: 'auth', version: '1' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/sign-in')
+  async signIn(@Body() body: SignInDto) {
+    return this.authService.signIn(body.email, body.password);
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import defaultPlainToClass from '@src/utils/functions/default.plain.to.class.fn';
+import { FindUserDto } from '../user/dto/find-user.dto';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '@src/providers/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
 
   generateToken(id: string, email: string, roles: string[] | string) {
     const payload = { id: id, email: email, roles: roles };
@@ -11,5 +15,19 @@ export class AuthService {
     const token = this.jwtService.sign(payload);
 
     return token;
+  }
+
+  async signIn(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) throw new BadRequestException('Incorrect email/password combination');
+
+    const passwordMatched = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatched) throw new BadRequestException('Incorrect email/password combination');
+
+    const token = this.generateToken(user.id, user.email, user.role);
+
+    return { user: defaultPlainToClass(FindUserDto, user), token };
   }
 }
