31651cdabb027c2a2e16663917e9abc4d98c2de1
Hariel <hariel@hotmail.com>
2022-05-06 08:33:26 -0300

    mgiration

--- prisma/migrations/20220503135003_fix_user/migration.sql
+++ prisma/migrations/20220503135003_fix_user/migration.sql
@@ -4,3 +4,5 @@ CREATE TYPE "enumUserRoles" AS ENUM ('owner', 'admin');
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "role" "enumUserRoles" NOT NULL DEFAULT E'owner',
 ALTER COLUMN "picture" DROP NOT NULL;
+
+UPDATE users SET role = 'admin' WHERE id ='fb1191a3-fc24-4fa4-9769-c9e205089057';
