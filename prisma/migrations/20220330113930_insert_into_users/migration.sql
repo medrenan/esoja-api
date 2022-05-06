9b796704b0ffcc6a6e26d532a69d656065a3e9fe
Hariel <hariel@hotmail.com>
2022-05-06 08:26:47 -0300

    migration.sql

--- prisma/migrations/20220330113930_insert_into_users/migration.sql
+++ prisma/migrations/20220330113930_insert_into_users/migration.sql
@@ -1,2 +1,2 @@
 -- InsertIntoUsers
-insert into "users" ("createdAt", "email", "id", "name", "passwordResetExpires", "passwordResetToken", "password", "picture", "updatedAt") values ('2022-03-30 11:25:02.06', 'admin@admin.com', 'fb1191a3-fc24-4fa4-9769-c9e205089057', 'admin', NULL, NULL, 'admin', '', '2022-03-30 11:24:44.03');
+insert into "users" ("createdAt", "email", "id", "name", "passwordResetExpires", "passwordResetToken", "password", "picture", "updatedAt") values ('2022-03-30 11:25:02.06', 'admin@admin.com', 'fb1191a3-fc24-4fa4-9769-c9e205089057', 'admin', NULL, NULL, '$2b$10$zq91nAH7lB8HYsoEFuGZNO6Pg6KkzJOtB6UXjtcIowSd25wyBmZnu', '', '2022-03-30 11:24:44.03');
