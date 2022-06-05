5be70c74721877aeeb1e8714a86d273b5e511b18
Hariel <hariel@hotmail.com>
2022-06-05 11:45:45 -0300

    agritec interfaces to dtos

--- src/providers/agritec/dto/agritec.cultivares.dto.ts
+++ src/providers/agritec/dto/agritec.cultivares.dto.ts
@@ -1,18 +1,36 @@
-export interface AgritecCultivaresI {
+import { Expose } from 'class-transformer';
+
+export class AgritecCultivaresDto {
+  @Expose()
   idCultivar: number;
+  @Expose()
   idCultura: number;
+  @Expose()
   safra: string;
+  @Expose()
   numeroRnc: string;
+  @Expose()
   obtentorMantenedor: string;
+  @Expose()
   cultivar: string;
+  @Expose()
   cultura: string;
+  @Expose()
   potencialProdutivo: number;
+  @Expose()
   duracaoCiclo: number;
+  @Expose()
   uf: string;
+  @Expose()
   grupo: string;
+  @Expose()
   maturacaoFisiologica: number;
+  @Expose()
   floracao: number;
+  @Expose()
   dataAtualizacao: string;
+  @Expose()
   regiao: string;
+  @Expose()
   grupoBioClimatico: string;
 }
--- src/providers/agritec/dto/agritec.produtividade.dto.ts
+++ src/providers/agritec/dto/agritec.produtividade.dto.ts
@@ -1,22 +1,43 @@
-export interface AgritecResponseProdutividadeI {
+import { Expose } from 'class-transformer';
+
+export class AgritecResponseProdutividadeDto {
+  @Expose()
   produtividadeAlmejada: number[];
+  @Expose()
   produtividadeMediaMunicipio: number[];
+  @Expose()
   temperaturaMinima: number[];
+  @Expose()
   temperaturaMaxima: number[];
+  @Expose()
   precipitacao: number[];
+  @Expose()
   grausDia: number[];
+  @Expose()
   balancoHidrico: number[];
+  @Expose()
   deficienciaHidrica: number[];
+  @Expose()
   excedenteHidrico: number[];
 }
-export interface AgritecProdutividadeI {
+
+export class AgritecProdutividadeDto {
+  @Expose()
   produtividadeAlmejada: number;
+  @Expose()
   produtividadeMediaMunicipio: number;
+  @Expose()
   temperaturaMinima: number;
+  @Expose()
   temperaturaMaxima: number;
+  @Expose()
   precipitacao: number;
+  @Expose()
   grausDia: number;
+  @Expose()
   balancoHidrico: number;
+  @Expose()
   deficienciaHidrica: number;
+  @Expose()
   excedenteHidrico: number;
 }
